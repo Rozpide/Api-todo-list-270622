f23cf38ba5a1a59123d6d403e4467fc6b4778aed
Rozpi <joserozpide@yahoo.es>
2022-06-27 23:45:46 +0000

    ok

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,52 @@
 
-const createUser = (name) => {
-const fetch = require('node-fetch');
+const createUser = () => {
+    const fetch = require('node-fetch');
 
-let url = 'http://assets.breatheco.de/apis/fake/todos/user/rozpi6';
+    let url = 'http://assets.breatheco.de/apis/fake/todos/user/rozpi6';
 
-let options = {
-  method: 'GET',
-  headers: {'Content-Type': 'application/json'},
-  body: '[{"label":"hacer la cama","done":false},{"label":"peinar al perro","done":false},{"label":"secar la ropa","done":false},{"label":"aprender python","done":false},{"label":"subir de nivel","done":false}]'
+    let options = {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+    // body: JSON.stringify([{label:"" , done: ""}]),
+    };
+
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));}
+
+const updateUser = () => {    
+
+    const fetch = require('node-fetch');
+
+    let url = 'http://assets.breatheco.de/apis/fake/todos/user/rozpi6';
+    
+    let options = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: '[{"label":"hacer la cama","done":false},{"label":"peinar al perro","done":false},{"label":"secar la ropa","done":false},{"label":"aprender python","done":false},{"label":"subir de nivel","done":false}]'
+    };
+    
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error('error:' + err));}
+
+const deleteUser = () => {
+
+      const fetch = require('node-fetch');
+
+      let url = 'http://assets.breatheco.de/apis/fake/todos/user/rozpi6';
+      
+      let options = {method: 'DELETE'};
+      
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error('error:' + err));}
+
+export default {
+    createUser,
+    deleteUser,
+    updateUser,
 };
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));}
-
